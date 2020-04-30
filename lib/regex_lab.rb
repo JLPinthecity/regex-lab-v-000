2722b60cf71e51fd72172d486a147f3e7439f849
Joann Pan <joannlpan@gmail.com>
2020-04-30 17:39:11 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-
+   phone.match(/^[0-9]*{10} | \W[0-9]{3}\W[0-9]{3}\W[0-9]{4} | \W[0-9]{3}\W[0-9]{7} /) ? true : false
 end
